1f1512501102b2bcffaf6ddde1686ff7474b0567
Lama Alfreah <lamaaalfreah@gmail.com>
2025-06-23 08:44:25 +0300

    Solution for the lab exceptions

---
+++ lab_exceptions.py
@@ -0,0 +1,12 @@
+def additoin(x, y):
+    try:
+        x = 10
+        y = 20
+        print("Addition:", x + b)
+        print("The operation is successful")
+    except NameError as e:
+        print(e)
+
+
+
+additoin(10, 20)
